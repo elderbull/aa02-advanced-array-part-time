9d7731304d425b56e433b7cb14cbb248daaf2285
Pristine <prisvsyo@gmail.com>
2024-02-10 12:23:06 -0800

    pristine test 'can the puller push'

--- problems/00-sum-forEach.js
+++ problems/00-sum-forEach.js
@@ -13,7 +13,7 @@ console.log(sumForEach([])); // 0
 
 let sumForEach = function(arr) {
   // Your code here
-  console.log()
+  console.log('can the puller push')
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
