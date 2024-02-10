d2f8f02df678cba9584692e8d946096f88bf58e7
Pristine <prisvsyo@gmail.com>
2024-02-10 12:48:51 -0800

    completed problem 1

--- problems/01-avg-val-forEach.js
+++ problems/01-avg-val-forEach.js
@@ -14,9 +14,24 @@ console.log(avgVal([])); // null
 */
 
 let avgVal = function(arr) {
-    // Your code here 
+    // Your code here
+    sum = 0;
+    let length = arr.length;
+    let avg;
+    if (length === 0) {
+      return null;
+    }
+
+    arr.forEach(function(num) {
+      sum += num;
+    })
+    avg = sum/length;
+    return avg;
 };
 
+console.log(avgVal([5, 10])); // 7.5
+console.log(avgVal([3, 7, 2, 1, 2])); // 3
+console.log(avgVal([])); // null
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
